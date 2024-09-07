be74b49d4af9db150cd62f4bd0bfa2a7bcf9f52d
Favour Idowu <favouridowu02@gmail.com>
2024-09-07 14:59:22 +0100

    Made few changes to the index.py module to handle some edge cases

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,12 +14,12 @@ from models.state import State
 from models.user import User
 
 count = {
-    "amenities": storage.count(Amenity),
-    "cities": storage.count(City),
-    "places": storage.count(Place),
-    "reviews": storage.count(Review),
-    "states": storage.count(State),
-    "users": storage.count(User)
+    "amenities": storage.count(Amenity) or 0,
+    "cities": storage.count(City) or 0,
+    "places": storage.count(Place) or 0,
+    "reviews": storage.count(Review) or 0,
+    "states": storage.count(State) or 0,
+    "users": storage.count(User) or 0
 }
 
 
