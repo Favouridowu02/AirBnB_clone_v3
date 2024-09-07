fc750805075c04fb2be8d275b3cc00058af8372b
Favour Idowu <favouridowu02@gmail.com>
2024-09-07 14:48:54 +0100

    Implemented the /api/v1/stats route. This Route returns the number of counts of each class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,32 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+count = {
+    "amenities": storage.count(Amenity),
+    "cities": storage.count(City),
+    "places": storage.count(Place),
+    "reviews": storage.count(Review),
+    "states": storage.count(State),
+    "users": storage.count(User)
+}
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Return status of the application"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """An EndPoint that retrieves the number of each objects by type"""
+    return jsonify(count)
