6a58dbe32e6ab9bc139d081fcb0a3de432e6bcb9
Favour Idowu <favouridowu02@gmail.com>
2024-09-28 18:33:00 +0100

    Added the api model for the review model
    Implemented the api model for the review api with route=/api/v1/views/reviews. The model contains the CRUD operations for the api endpoint

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -3,6 +3,7 @@
 from api.v1.views import app_views
 from models.review import Review
 from models.place import Place
+from models.user import User
 from models import storage
 from flask import abort, jsonify, request
 
@@ -46,5 +47,34 @@ def delete_reviews(review_id=None):
 def post_review(place_id=None):
     """This Method creates a new review based on the review_id"""
     if request.content_type != "application/json":
-        jsonify({"error": "Not a JSON"})
-        
+        jsonify({"error": "Not a JSON"}), 400
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    data = request.get_json()
+    if "user_id" not in data.keys():
+        return jsonify({"error":"Missing user_id"}), 200
+    user = storage.get(User, data["user_id"])
+    if not user:
+        abort(404)
+    if "text" not in data.keys():
+        return jsonify({"error": "Missing text"}), 400
+    review = Review(**data)
+    review.save()
+    return jsonify(review.to_dict()), 201
+
+
+@app_views.route("/api/v1/reviews/<review_id>", strict_slashes=True, methods=["PUT"])
+def put_review(review_id=None):
+    """This Method updates the data of review based on the review_id"""
+    review = storage.get(Review, review_id)
+    if not review_id or review:
+        abort(404)
+    if request.content_type != "applicatioon/json":
+        return jsonify({"error": "Not a JSON"})
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ["id", "user_id", "place_id", "created_at"]:
+            setattr(review, key, value)
+    review.save()
+    return jsonify(review.to_dict()), 200
